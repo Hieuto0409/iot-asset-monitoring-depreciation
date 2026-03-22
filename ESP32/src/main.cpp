b5fab74614c6747ed36d5bec02a10f19ba352c04
Hieu <kid074334@gmail.com>
2026-03-22 10:30:47 +0700

    first code

---
+++ ESP32/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#define LED_GPIO 22
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_GPIO,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_GPIO,HIGH);
+  delay(1000);
+  digitalWrite(LED_GPIO,LOW);
+  delay(1000);
+}
+
